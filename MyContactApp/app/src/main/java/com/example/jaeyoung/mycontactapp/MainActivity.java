9911363578f7d04e3c30b14f09c4dd32dd50621e
BeatStanford <choijaeyoung38@gmail.com>
2017-05-16 11:05:49 -0700

    Added getAllData method and button to retrieve contacts. Jaeyoung Choi Period 2

--- MyContactApp/app/src/main/java/com/example/jaeyoung/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/jaeyoung/mycontactapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jaeyoung.mycontactapp;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,12 +31,34 @@ public class MainActivity extends AppCompatActivity {
         boolean isInserted = myDb.insertData(editName.getText().toString());
         if(isInserted == true) {
             Log.d("My Contact", "Successfully inserted data");
-            //Insert Toast message here:
+            //Insert Toast message here to indicate data was inserted successfully:
             Toast.makeText(this, "Contact added", Toast.LENGTH_SHORT).show();
         }
         else {
             Log.d("My Contact", "Error inserting data");
+            //Create toast message saying it failed.
+            Toast.makeText(this, "Error inserting Data", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void viewData (View v) {
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0) {
+            showMessage("Error", "No data found in database");
+            //Output a message using log.d and Toast
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        /*setup a loop with the cursor (res), using method moveToNext
+        *inside the loop, append each column to the buffer
+        *look for a method in the string buffer class that lets you append id, name, email, etc.
+        * display the message using the showMessage
+        */
+        showMessage("Data", buffer.toString());
+    }
+
+    private void showMessage(String error, String s) {
+        //AlertDialog.Builder
     }
 
 
